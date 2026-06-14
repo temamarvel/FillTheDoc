e4e3a57ffeb7a516846cb33c462e810eb69b4bfa
temamarvel <tema.denisoff@gmail.com>
2026-06-14 14:17:59 +0400

    fix error hightliting

--- FillTheDoc/Features/PlaceholderLibrary/LabeledTextFieldView.swift
+++ FillTheDoc/Features/PlaceholderLibrary/LabeledTextFieldView.swift
@@ -38,40 +38,50 @@ struct LabeledTextFieldView<Label: View>: View {
         return true
     }
     
+    private var isMultiline: Bool {
+        minLines > 1
+    }
+    
     var body: some View {
         VStack(alignment: .leading) {
             labelContent
             
-            
-            VStack(alignment: .trailing) {
+            VStack(spacing: 0) {
                 TextField(prompt, text: $text, axis: .vertical)
                     .lineLimit(minLines...)
                 
                 if showError, let error = error {
-                    Text(error)
-                        .foregroundStyle(.red)
-                        .font(.subheadline)
-                        .padding(.trailing, 4)
-                        .padding(.bottom, 2)
-                        .transition(.opacity.combined(with: .move(edge: .top)))
+                    HStack{
+                        Spacer()
+                        
+                        Text(error)
+                            .foregroundStyle(.red)
+                            .font(.subheadline)
+                            .padding(.trailing, 4)
+                            .padding(.top, isMultiline ? 12 : 8)
+                            .padding(.bottom, 2)
+                    }
+                    .zIndex(-1)
+                    .transition(.opacity.combined(with: .move(edge: .top)))
+                    .background(
+                        LinearGradient(
+                            colors: [
+                                .red.opacity(0.3),
+                                .red.opacity(0.1),
+                                .clear
+                            ],
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                        .cornerRadius(4)
+                        .opacity(showError ? 1 : 0)
+                        .animation(.easeInOut(duration: 0.2), value: showError)
+                    )
+                    
+                    .offset(y: isMultiline ? -10 : -6)
                 }
             }
-            .background(
-                LinearGradient(
-                    colors: [
-                        .clear,
-                        .red.opacity(0.1),
-                        .red.opacity(0.3),
-                        .clear
-                    ],
-                    startPoint: .bottom,
-                    endPoint: .top
-                )
-                
-                .cornerRadius(4)
-                .opacity(showError ? 1 : 0)
-                .animation(.easeInOut(duration: 0.2), value: showError)
-            )
+            
         }
     }
 }
@@ -106,7 +116,10 @@ extension LabeledTextFieldView where Label == Text {
             .padding()
             .disabled(true)
         
+        LabeledTextFieldView(text: .constant("Пример текста Пример текста Пример текста Пример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текста"), prompt: "prompt", label: "LAbel", error: "Error", minLines: 3)
+            .padding()
         
+        Spacer()
     }
-    .frame(width: 300, height: 300)
+    .frame(width: 300, height: 500)
 }
